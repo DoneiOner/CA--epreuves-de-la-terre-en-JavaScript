58f10462de2395027c25e3b4a9abb4fc68eb0fab
donei <donei.dev@gmail.com>
2022-01-13 17:59:09 +0100

    exo 01

---
+++ terre01.js
@@ -0,0 +1,22 @@
+#!usr/bin/env ruby
+
+/*
+************************* Nom du programme
+    Créez un programme qui affiche son nom de fichier.
+
+Exemples d’utilisation :
+$> node exo.js
+exo.js
+
+$> node crevette.js
+crevette.js
+    
+*/
+
+var path = require('path');
+var scriptname = path.basename(__filename); 
+console.log(scriptname);
+
+
+
+
