0f90183c90cd089eed7b858bcd87b860a90b946f
donei <donei.dev@gmail.com>
2022-01-15 12:19:40 +0100

    exo 09

---
+++ terre09.js
@@ -0,0 +1,36 @@
+/*
+      ******************* Racine carrée d’un nombre
+  
+Créez un programme qui affiche la racine carrée d’un entier positif.
+
+Exemples d’utilisation :
+$> node exo.js 9
+3
+
+Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
+
+
+
+puts Math.sqrt(nombre1.to_i)
+
+*/
+
+// gestion des erreurs
+if (process.argv[2] == null) {
+    console.log ('Erreur.');
+    return;
+}
+
+var test_saisie = new RegExp('^[0-9]+$');
+//console.log(test_saisie.test(saisie[index]));
+if ((!test_saisie.test(process.argv[2]) || process.argv[3] != null)){
+    console.log('tu pensais me la mettre? => Erreur.');
+    return;
+}
+
+
+// parsing
+var nombre = process.argv[2];
+
+// resolution
+console.log(Math.sqrt(nombre));
