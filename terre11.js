cd0753c235efefbbc360d47b3befe2dc01d7d7cc
donei <donei.dev@gmail.com>
2022-01-17 12:25:24 +0100

    exo 11

---
+++ terre11.js
@@ -0,0 +1,85 @@
+#!usr/bin/env ruby
+
+/*
+    
+
+************************************ 24 to 12
+
+    Créez un programme qui transforme une heure affichée en format 24h 
+    en une heure affichée en format 12h.
+
+Exemples d’utilisation :
+$> ruby exo.rb 23:40
+11:40PM
+
+Attention : midi et minuit.
+
+    
+
+
+ recuperer argument en format 24h
+ splitter les heures et les minutes
+
+minuit : 12AM (midnight)                       ======> ex : 12:15 AM                   
+de 01:00 à 11:59=> AM                          ==========>    afficher ARGV[0] + "PM"
+midi  12PM (noon)  
+de 12:01 à 12:59 => PM                         ===========>   afficher ARGV[0] + "PM"
+de 13:00 à 23:59 => PM                        ===========> afficher ARGV[0] - 12 + "PM"
+
+
+ afficher en format 12h
+=end
+
+
+*/
+
+// gestion des erreurs
+if (process.argv[2] == null) {
+    console.log ('Erreur.');
+    return;
+}
+
+var test_saisie = new RegExp('^[:0-9]+$');
+if ((!test_saisie.test(process.argv[2]) || process.argv[3] != null)){
+    console.log(' Erreur.');
+    return;
+}
+
+
+// parsing
+var saisie = process.argv[2];
+//console.log(saisie);
+
+tableau = saisie.split(':');
+//console.log(tableau);
+
+var heure_saisie = tableau[0];
+var minute_saisie = tableau[1];
+//console.log(heure_saisie + " " + minute_saisie);
+
+var sortie;
+var heure_sortie = heure_saisie;
+
+// resolution
+if (heure_saisie == 00) {
+    heure_sortie = 12;
+    sortie = "AM";
+}
+if (heure_saisie == 12) {
+        sortie = "PM";
+    }
+   else if  (13 <= heure_saisie && heure_saisie <= 23) { 
+        heure_sortie -= 12;
+        sortie = "PM";
+   }
+    else if (0 <=  heure_saisie && heure_saisie <= 11 ){
+        sortie = "AM"; 
+    }
+    else {
+        console.log('erreur.');
+        return;
+    }  
+
+// affichage
+console.log(heure_sortie + ':' + minute_saisie + sortie);
+
