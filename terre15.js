2d42c5085f61a08c4c947aed2827b6a190595cb8
donei <donei.dev@gmail.com>
2022-01-18 07:51:56 +0100

    exo 15

---
+++ terre15.js
@@ -0,0 +1,20 @@
+#!usr/bin/env ruby
+
+/*
+   ************************** Terre : ok 
+
+Créez un programme qui célèbre votre victoire.
+
+Exemples d’utilisation :
+$> ruby exo.rb
+J’ai terminé l’Épreuve de la Terre et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+*/
+
+console.log("Donei Oner a terminé l'épreuve de la Terre le dimanche 9 janvier 2K22 et c'était STI-MU-LANT et RAGEANT ... ENCOU-RAGEANT");
+console.log(" (enfin, peut-être un peu moins que la voisine qui me glisse son numéro pour se voir samedi dernier ...");
+ console.log("mais on dit que la vie est une question de priorités et je compte bien remplir mes objectifs dorénavant");
+ console.log(" peut-être une autre fois, Géraldine..!");
